2899cfb71dc9daf50389d4d4a6552dd71b6a03e3
Taha Sanay <79981074+TahaSanay@users.noreply.github.com>
2022-10-05 23:44:47 +0300

    Add files via upload

---
+++ oopWithNLayerApp/src/oopWithNLayerApp/core/logging/MailLogger.java
@@ -0,0 +1,10 @@
+package oopWithNLayerApp.core.logging;
+
+public class MailLogger implements Logger {
+
+	@Override
+	public void log(String data) {
+		System.out.println("Mail Gonderildi :" + data);
+
+	}
+}
